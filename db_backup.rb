b96d0c123f342a6add48ebf68e71e4534ac7d921
JonMidhir <info@midhirrecords.com>
2013-04-24 19:00:06 +0100

    Clean up variable names and file paths

--- db_backup.rb
+++ db_backup.rb
@@ -3,28 +3,30 @@ require 'aws/s3'
 require 'yaml'
 
 config = YAML.load_file(File.expand_path("../config.yml", __FILE__))
-project_config, amazon_config = config["project"], config["amazon"]
+project, amazon = config["project"], config["amazon"]
 
 AWS::S3::Base.establish_connection!(
-    :access_key_id     => amazon_config['access_key_id'],
-    :secret_access_key => amazon_config['secret_access_key']
+    :access_key_id     => amazon['access_key_id'],
+    :secret_access_key => amazon['secret_access_key']
   )
   
-database = YAML.load_file("#{project_config['base_path']}/config/database.yml")[project_config['env']]
-dumpfile = File.expand_path("/tmp/#{database['database']}_backup", __FILE__)
+database = YAML.load_file("#{project['base_path']}/config/database.yml")[project['env']]
 
-print "Creating dumpfile (#{dumpfile})..."
+filename = "#{database['database']}_#{Time.now.strftime('%F-%H.%M')}.backup"
+dumpfile = File.expand_path("/tmp/#{filename}", __FILE__)
+
+print "Creating dumpfile (#{filename})..."
 command = "mysqldump --user #{database['username']} --password=#{database['password']} #{database['database']} > #{dumpfile}"
 
 r = system(command)
 puts "done."
 
 print "Sending backup to AWS..."
-send = AWS::S3::S3Object.store("#{database['database']}_#{Time.now.strftime('%F-%H.%M')}.backup", open(dumpfile), amazon_config['bucket'])
+s3_object = AWS::S3::S3Object.store("#{filename}", open(dumpfile), amazon['bucket'])
 
 puts "done."
 
-if send.response.code == "200"
+if s3_object.response.code == "200"
   puts "Dumpfile deleted." if File.delete("#{dumpfile}") == 1
 else
   puts "WARNING: Dumpfile not deleted."
